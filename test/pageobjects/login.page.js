3f917bddd262100a8c777619789c859fd4e56c3c
Sebastian sileoni <seba20@gmail.com>
2021-05-10 22:37:13 -0300

    completed the login pages structure

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -11,22 +11,22 @@ class LoginPage {
 
   /*selector for Email error*/
   get errorEmail () {
-    return $('input[id="loginErrorEmail"]');
+    return $('div[id="loginErrorEmail"]');
   }
 
   /*selector for Password error*/
   get errorPassword () {
-    return $('input[id="loginErrorPassword"]');
+    return $('div[id="loginErrorPassword"]');
   }
 
   /*selector for the Register button*/
   get btnRegister {
-    return $('input[id="backToRegister"]');
+    return $('button[id="backToRegister"]');
   }
 
   /*selector for login button*/
   get btnLogin {
-    return $('input[id="loginButton"]');
+    return $('button[id="loginButton"]');
   }
 
   // /*selector for clean form  function*/
